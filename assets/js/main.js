668532426575605b99e2fce5534b8d16f533359b
diego-cavenati <diegocavenati@gmail.com>
2022-10-17 15:52:46 +0200

    generate grid with number and click event

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,48 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 
 // click dell'utente
 
+
+//prendo elemento grid
+const gridEl = document.querySelector(`.grid`)
+// const containerEl = document.querySelector(`.container`)
+
+// decido grandezza griglia
+const cellsNumber = 100;
+
+// generazione singola cell
+function generateMarkup(numb) {
+    const cellElement = document.createElement('div')
+    cellElement.className = "cell"
+    cellElement.innerText = numb;
+    return cellElement
+}
+
+
+
 // generazione griglia di gioco quadrata 10x10
+generateField(cellsNumber, gridEl);
+function generateField(max, domEl) {
+
+    for (let i = 0; i <= cellsNumber; i++) {
+
+        //const thisNumber = cellsNumber[i];
+        const cellElement = generateMarkup(i);
+
+        // inserisci il markup nel container
+        domEl.insertAdjacentElement(`beforeend`, cellElement);
+
+        // al click dell'utente la casella diventa blu
+        cellElement.addEventListener('click', function(){
+            
+            this.classList.toggle("active")
+            console.log(this);
+
+        }
+        )
+        
+    }
+
+}
+
 
 // ogni cella ha un numero progressivo da 1 a 100
